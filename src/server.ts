aeeba497536001ddb02849367b59423053f5cbc7
Artur Rodrigues <artur190503@outlook.com>
2024-04-03 13:21:24 -0300

    Criado banco de dados prisma e feitas primeiras requisições com REST

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,33 @@
 import fastify from "fastify";
+import { z } from "zod";
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify();
 
-app.get('/', () => {
-    return 'Hello World'
+const prisma = new PrismaClient({
+    log: ['query']
+});
+
+app.post('/events', async (request, reply) => {
+
+    const createEventSchema = z.object({
+        tittle: z.string().min(4),
+        details: z.string().nullable(),
+        maximunAttendees: z.number().int().positive().nullable(),
+    })
+
+    const data = createEventSchema.parse(request.body);
+
+    const event = await prisma.event.create({
+        data: {
+            tittle: data.tittle,
+            details: data.details,
+            maximumAttendees: data.maximunAttendees,
+            slug: new Date().toISOString()
+        }
+    })
+
+    return reply.code(201).send({eventId : event.id});
 })
 
 app.listen({ port: 3333}).then(() => {
